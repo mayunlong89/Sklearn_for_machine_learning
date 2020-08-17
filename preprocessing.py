c307b3f7a0a2c5fc2f6067c58d71a3d9f4a1d4e7
Yunlong Ma <11418032@zju.edu.cn>
2020-08-18 00:30:37 +0800

    updated

---
+++ preprocessing.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 17 23:05:25 2020
+"""
+
+
+#preprocessing data
+#scale data
+from sklearn import preprocessing
+import numpy as np
+
+a = np.array([[10,2.7,3.6],
+             [-100,5,-2],
+             [120,20,40]], dtype=np.float64)
+
+print(a)
+print(preprocessing.scale(a))
+
