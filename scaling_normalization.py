81b12b747d7bf206a39a8417c9350be7d2d31142
Yunlong Ma <11418032@zju.edu.cn>
2020-08-26 10:24:57 +0800

    Update scaling_normalization.py

--- scaling_normalization.py
+++ scaling_normalization.py
@@ -5,6 +5,8 @@ Created on Mon Aug 17 23:05:25 2020
 @author: Yunlong Ma
 """
 
+#---------------------Start-----------------------------------------------
+
 from sklearn.model_selection import train_test_split
 from sklearn.datasets.samples_generator import make_classification
 from sklearn.svm import SVC
@@ -24,3 +26,9 @@ X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size=0.3)
 clf =SVC()
 clf.fit(X_train,Y_train)
 print(clf.score(X_test,Y_test))
+
+
+
+
+#------------------End--------------------------------------------------------
+
